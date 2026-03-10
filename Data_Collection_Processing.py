e2bedec9967a70dd7faea797e5c831885ab9000a
Charles-Weller <charlesweller.ccw@gmail.com>
2026-03-10 11:28:28 +0000

    Update Data_Collection_Processing.py

--- Data_Collection_Processing.py
+++ Data_Collection_Processing.py
@@ -1,27 +1,56 @@
-def GatherPatientData(fileName, p):#p holds if the data is processed or not
-    data = open(fileName, "r").read().splitlines()
-    if p == 0:
-        patientData = []
-        tempPatient = []
+def GatherPatientData(fileName, processed):#p holds if the data is processed or not
+    fileData = open(fileName, "r").read().splitlines()
+    patientData = []
 
-        for line in data:
-            for tok in line.split():
-                tempPatient.append(tok)
+    if processed == False:
+        tempPatient = []
+        for line in fileData:
+            for featureInfo in line.split():
+                tempPatient.append(featureInfo)
                 if len(tempPatient) == 76:
                     patientData.append(tempPatient)
                     tempPatient = []
     else:
-        print("add code")
+        for line in fileData:
+            patientData.append(line.split(","))
 
-    print(patientData)
     return(patientData)
 
+def FeatureInfermation(patientData, fileType):
+    match(fileType):
+        case 0:
+            length = 76
+        case 1:
+            length = 14
+        case _:
+            print("Enter the number of features within your data")
+            length = int(input("       : "))
+    
+    totalData = [[] for _ in range(length)]
+    for i in range(len(patientData)):
 
-def ChoiceWantedFiles():
+        for l in range(length):
+            try:
+                dataPoint = float(patientData[i][l])
+            except:
+                dataPoint = patientData[i][l]
+
+            found = False
+            for k in range(len(totalData[l])):
+                if totalData[l][k][0] == dataPoint:
+                    totalData[l][k][1] += 1
+                    found = True
+                    break
+
+            if not found:
+                totalData[l].append([dataPoint, 1])
+                
+def GatherAllPatientData():
     totalFilesData = []
     chosen = False
     print("Enter the ID of the file you want to get data from:")
     print("Enter 0: When all wanted files have been chosen")
+    print("The applcation will not work if unprocessed and processed files are used in tandom")
     print()
     print("Enter 1: for cleveland data")
     print("Enter 2: for hungarian data")
@@ -39,28 +68,37 @@ def ChoiceWantedFiles():
             case 0:
                 break
             case 1:
-                fileData = GatherPatientData("cleveland - Copy.data", 0)
+                fileData = GatherPatientData("cleveland - Copy.data", False)
+                fileType = 0
             case 2:
-                fileData = GatherPatientData("hungarian.data", 0)
+                fileData = GatherPatientData("hungarian.data", False)
+                fileType = 0
             case 3:
-                fileData = GatherPatientData("long-beach-va.data", 0)
+                fileData = GatherPatientData("long-beach-va.data", False)
+                fileType = 0
             case 4:
-                fileData = GatherPatientData("switzerland.data", 0)
+                fileData = GatherPatientData("switzerland.data", False)
+                fileType = 0
             case 5:
-                fileData = GatherPatientData("processed.cleveland.data", 1)
+                fileData = GatherPatientData("processed.cleveland.data", True)
+                fileType = 1
             case 6:
-                fileData = GatherPatientData("processed.hungarian.data", 1)
+                fileData = GatherPatientData("processed.hungarian.data", True)
+                fileType = 1
             case 7:
-                fileData = GatherPatientData("processed.va.data", 1)
+                fileData = GatherPatientData("processed.va.data", True)
+                fileType = 1
             case 8:
-                fileData = GatherPatientData("processed.switzerland.data", 1)
+                fileData = GatherPatientData("processed.switzerland.data", True)
+                fileType = 1
             case 9:
                 print("Enter the name of the file you would like to get data from")
                 print("the file type will also need to be added (e.g: .csv, .data, ...)")
                 fileName = input("       : ")
-                fileData = GatherPatientData(fileName, 1)#Code assumes that use files are in comma seperated and all patient data is on a single row
+                fileData = GatherPatientData(fileName, True)#Code assumes that use files are in comma seperated and all patient data is on a single row
+                fileType = 2
         totalFilesData += fileData
-        print("hi")
+    return(totalFilesData, fileType)
 
 def Menu():
     running = True 
@@ -70,24 +108,25 @@ def Menu():
     print("Then the code will ask whcih files you could like to use data from")
     print()
     while running == True:
-        print("Enter  1: To collect the feature options and population")
-        print()
-        print("Enter  0: To stop the running of this applcation")
+        print("Enter 0: To stop the running of this applcation")
+        print("Enter 1: To collect the feature quantative and population")
+        print("Enter 2: To collect a chosen feature quantative and population")
+        print("Enter 3: To collect feature quantative and population based off stage clasifiaction")
+        print("Enter 4 ")
         choice = int(input("       : "))
 
-        patientData = ChoiceWantedFiles()
+        patientData, fileType = GatherAllPatientData()
         match(choice):
             case 1:
+                FeatureInfermation(patientData, fileType)
+            case 2:
+                print("add code")
+            case 3:
                 print("add code")
 
     print("Thanks you for using this appcation")
     print("Have a greate day")
 
-
-    print("Enter 4:  To collect data on a peticular feture from all unprocessed file")
-    print("Enter 5:  To collect data on a peticular feature from all processed file")
-    print("Enter 6:  To collect data on a peticular feature from one file")
-    print("Enter 7:  To collect all data from one file based off patient output")## Add options such as for a indevdule featrure 
     print("Enter 8:  To detect all missing or incorrect data from all unprocessed files")
     print("Enter 9:  To detect all missing and incorrect data from all processed files")
     print("Enter 10: To collect all missing and incorrect data from one file")
