343de3f4586b9b2ed8b71d9a459f4d74dd2d63a7
Charles-Weller <charlesweller.ccw@gmail.com>
2026-03-17 15:14:52 +0000

    Update Data_Collection_Processing.py

--- Data_Collection_Processing.py
+++ Data_Collection_Processing.py
@@ -31,7 +31,6 @@ def FeatureInfermation(patientData, fileType, function):
     processedData = [[] for _ in range(length)]
     patientCount = 0
     for i in range(len(patientData)):
-
         for l in range(length):
             try:
                 dataPoint = float(patientData[i][l])
@@ -57,28 +56,33 @@ def FeatureInfermationDeseaseClassifiaction(patientData, fileType, function):
     match(fileType):
         case 0:
             length = 76
+            classificationPosition = 57
         case 1:
             length = 14
+            classificationPosition = 13
         case _:
             print("Enter the number of features within your data")
             length = int(input("       : "))
+
+            print("Enter the feature ID for the classification of the patient")
+            classificationPosition = int(input("       : ")) - 1
     
     processedData = [[[] for _ in range(length)] for _ in range(5)]
     patientCount = 0
     for i in range(len(patientData)):
 
-        if patientData[i][57] == "0":
+        if patientData[i][classificationPosition] == "0":
             n = 0
-        elif patientData[i][57] == "1":
+        elif patientData[i][classificationPosition] == "1":
             n = 1
-        elif patientData[i][57] == "2":
+        elif patientData[i][classificationPosition] == "2":
             n = 2
-        elif patientData[i][57] == "3":
+        elif patientData[i][classificationPosition] == "3":
             n = 3
         else:
             n = 4
 
-        for l in range(76):
+        for l in range(length):
             try:
                 dataPoint = float(patientData[i][l])
             except:
@@ -140,34 +144,47 @@ def MissingData(patientData, fileType, function):
     else:
         return updatedData, patientCount
 
-def FeatureStatistics(data, fileType):
-    import matplotlib.pyplot as plt
+def FeatureStatistics(data, fileType, function):
     classifiedData, _ = FeatureInfermationDeseaseClassifiaction(data, fileType, 1)
 
-    print("Enter the feature number you would like to collect the data on")
-    print("Enter 0 if you would like to recive all feature data")
-    featureChoice = int(input(("       : ")))
-    print()
+    if function == 0:
+        print("Enter the feature number you would like to collect the data on")
+        print("Enter 0 if you would like to recive all feature data")
+        featureChoice = int(input("       : "))
+        print()
 
-    print("Enter 1 if you would like to show a scatter graph")
-    print("Enter 0 otherwise")
-    showGraph = int(input("       : "))
-    print()
+        print("Enter graph type:")
+        print("Enter 0: To not display any graph")
+        print("Enter 1: To display a Scatter graph")
+        print("Enter 2: To display a Histogram")
+        print("Enter 3: To display a Box plot")
+        print("Enter 4: To display a Bar chart (average per classification)")
+        print("Enter 5: To display a Heatmap")
+        showGraph = int(input("       : "))
+        print()
+    else:
+        featureChoice = function
+        showGraph = 0
 
-    if featureChoice == 0:
+    if featureChoice == 0 or function != 0:
         length = len(classifiedData[0])
     else:
         length = 1
+
+    FunctionAverages = [[0 for _ in range(length)] for _ in range(5)]
 
     for i in range(length):
         if length == 1:
             i = featureChoice - 1
 
-        print("     Data for, feature", i + 1)
-        print("Classification statistics:")
+        if function == 0:
+            print("     Data for, feature", i + 1)
+            print("Classification statistics:")
 
         xValues = []
         yValues = []
+        groupedValues = []
+        barAverages = []
 
         for j in range(5):
             classNumericValues = []
@@ -177,59 +194,166 @@ def FeatureStatistics(data, fileType):
                     value = float(classifiedData[j][i][l][0])
                     count = classifiedData[j][i][l][1]
 
-                    for k in range(count):
+                    for _ in range(count):
                         classNumericValues.append(value)
                         xValues.append(j)
                         yValues.append(value)
                 except:
                     pass
 
+            groupedValues.append(classNumericValues)
+
             if len(classNumericValues) == 0:
-                print("Classification", j, ": No valid numeric data")
-                print()
+                if function == 0:
+                    print("Classification", j, ": No valid numeric data")
+                    print()
+                barAverages.append(0)
             else:
-                classAverage = sum(classNumericValues) / len(classNumericValues)
+                classAverage = np.mean(classNumericValues)
+                FunctionAverages[j][i] = classAverage
                 classMinimum = min(classNumericValues)
                 classMaximum = max(classNumericValues)
-                classRange = classMaximum - classMinimum
 
-                print("Classification", j)
-                print("Average:", round(classAverage, 4))
-                print("Min:", classMinimum)
-                print("Max:", classMaximum)
-                print("Range:", classRange)
-                print("Valid data points:", len(classNumericValues))
-                print()
+                if function == 0:
+                    print("Classification", j)
+                    print("Average:", round(classAverage, 4))
+                    print("Standard Deviation:", round(np.std(classNumericValues), 4))
+                    print("Variance:", round(np.var(classNumericValues), 4))
+                    print("Min:", classMinimum)
+                    print("Max:", classMaximum)
+                    print("Range:", classMaximum - classMinimum)
+                    print("Valid data points:", len(classNumericValues))
+                    print()
 
-        if showGraph == 1:
+                barAverages.append(classAverage)
+
+        if showGraph != 0:
+
             if len(yValues) == 0:
                 print("No valid numeric data available to plot for feature", i + 1)
                 print()
             else:
-                plt.figure()
-                plt.scatter(xValues, yValues)
-                plt.title("Feature " + str(i + 1) + " by Classification")
-                plt.xlabel("Classification")
-                plt.ylabel("Feature Value")
-                plt.xticks([0, 1, 2, 3, 4])
+                import matplotlib.pyplot as plt
+                if showGraph == 1:
+                    plt.scatter(xValues, yValues)
+                    plt.title("Feature " + str(i + 1) + " by Classification")
+                    plt.xlabel("Classification")
+                    plt.ylabel("Feature Value")
+                    plt.xticks([0,1,2,3,4])
+
+                elif showGraph == 2:
+                    plt.hist(yValues, bins=10)
+                    plt.title("Histogram for Feature " + str(i + 1))
+                    plt.xlabel("Feature Value")
+                    plt.ylabel("Frequency")
+
+                elif showGraph == 3:
+                    plt.boxplot(groupedValues, tick_labels=[0,1,2,3,4])
+                    plt.title("Box Plot for Feature " + str(i + 1) + " by Classification")
+                    plt.xlabel("Classification")
+                    plt.ylabel("Feature Value")
+
+                elif showGraph == 4:
+                    plt.bar([0,1,2,3,4], barAverages)
+                    plt.title("Average Feature " + str(i + 1) + " by Classification")
+                    plt.xlabel("Classification")
+                    plt.ylabel("Average Value")
+
+                elif showGraph == 5:
+
+                    heatData = []
+
+                    for group in groupedValues:
+                        if len(group) == 0:
+                            heatData.append([0]*40)
+                        else:
+                            hist, _ = np.histogram(group, bins=40)
+                            heatData.append(hist)
+
+                    heatData = np.array(heatData)
+
+                    plt.figure(figsize=(12,6))
+
+                    plt.imshow(heatData, aspect='auto', cmap='inferno', interpolation='bicubic')
+                    plt.colorbar(label="Frequency")
+                    plt.title("Heatmap of Feature " + str(i + 1))
+                    plt.xlabel("Feature Value Bins")
+                    plt.ylabel("Classification")
+                    plt.yticks([0,1,2,3,4])
+
+                    plt.show()
+
+                else:
+                    print("Invalid graph option selected.")
+                    plt.close()
+                    continue
+
                 plt.show()
+
+    if function != 0:
+        return FunctionAverages
 
     print()
 
+def PatientDataUpdate(patientData, fileType):
+    match(fileType):
+        case 0:
+            length = 76
+            classificationPosition = 57
+            NON = [8,9,11,19,20,21,27,28,30,31,32,33,34,35,36,41,42,44,45,50,54,55,56,59,60,61,62,63,64,65,66,67,73]
+        case 1:
+            length = 14
+            classificationPosition = 14
+            NON = [2,3,4]
+        case _:
+            print("Enter the number of features within your data")
+            length = int(input("       : "))
+
+            print("Enter the feature ID for the classification of the patient")
+            classificationPosition = int(input("       : ")) - 1
+
+            enter =  False
+            NON = []
+            print("Enter the feature number where the it is not posible for the value to be 0")
+            print("Enter 0 : When you are enteded all feature id's needed")
+            print("Enter -1: To see the enteded features")
+            while enter == False:
+                featureChoice = int(input("       : "))
+
+                if featureChoice == -1:
+                    NON = sorted(set(NON))
+                    print(NON)
+                elif featureChoice == 0:
+                    enter = True
+                else:
+                    NON.append(featureChoice)
+
+    FunctionAverages = FeatureStatistics(patientData, fileType, 1)
+    for i in range(len(patientData)):
+        for j in range(length):
+            if j in NON and (patientData[i][j] == "0.0" or patientData[i][j] == "0"):
+                patientData[i][j] = FunctionAverages[int(patientData[i][classificationPosition])][j]
+            elif patientData[i][j] == "-9.0" or patientData[i][j] == "-9" or patientData[i][j] == "?":
+                 patientData[i][j] = FunctionAverages[int(patientData[i][classificationPosition])][j]
+    return patientData
 
 def Normalisation(x: np.ndarray):# Min Max 
     x = np.asarray(x, dtype=np.float64)
 
-    min = np.min(x)
-    max = np.max(x)
-    range = max - min
+    minimum  = np.min(x)
+    differance = np.max(x) - minimum
 
-    if np.isclose(range, 0.0):
+    if np.isclose(differance, 0.0):
         return np.zeros_like(x, dtype=np.float64)
     
-    return (x - min) / range
+    return (x - minimum ) / differance
 
-def SaveNewData(patientData, fileType):   
+def SaveNewData(patientData, fileType):
+    print("Enter 1: To replaces missing or incorrect data with the clasifiction average")
+    correctionchoice = int(input("       : "))
+    if correctionchoice == 1:
+        patientData = PatientDataUpdate(patientData, fileType)
+
     enter =  False
     wantedFeature = []
     print("Enter the feature number you would like to remove")
@@ -257,12 +381,10 @@ def SaveNewData(patientData, fileType):
     try:
         if normChoice == 1:  
             data = np.asarray(patientData, dtype=np.float64)
-        for j in range(data.shape[1]):
-            data[:, j] = Normalisation(data[:, j])
+            for j in range(data.shape[1]):
+                data[:, j] = Normalisation(data[:, j])
 
-            patientData = data.tolist()
-        else:
-            pass
+                patientData = data.tolist()
     except:
         print()
         print("Normalisation failed due to the presence of non-numeric data")
@@ -275,7 +397,7 @@ def SaveNewData(patientData, fileType):
                        "exerckm", "restef", "restwm", "exeref", "exerwm", "thal", "thalsev", "thalpul", "earlobe", "cmo", "cday", "cyr", "num", "lmt", "ladprox", 
                        "laddist", "diag", "cxmain", "ramus", "om1", "om2", "rcaprox", "rcadist", "lvx1", "lvx2", "lvx3", "lvx4", "lvf", "cathef", "junk", "name"]
     elif fileType == 1:
-        columnNames
+        columnNames = ["age","sex","cp","trestbps","chol","fbs","restecg","thalach","exang","oldpeak","slope","ca","thal","num"]
     else:
         enter =  False
         columnNames = []
@@ -326,7 +448,7 @@ def DataOutput(data, count, classification):
             if choice == 1:
                 sortedData = sorted(data[featureChoice-1] if length == 1 else data[i],key=lambda x: x[0])
             else:
-                sortedData = sorted(data[featureChoice-1] if length == 1 else data[i],key=lambda x: x[0])
+                sortedData = sorted(data[featureChoice-1] if length == 1 else data[i],key=lambda x: x[1])
         except:
             match(choice):
                 case 1:
@@ -344,7 +466,6 @@ def DataOutput(data, count, classification):
 
 def GatherAllPatientData():
     totalFilesData = []
-    chosen = False
     print("Enter the ID of the file you want to get data from:")
     print("Enter 0: When all wanted files have been chosen")
     print("The applcation will not work if unprocessed and processed files are used in tandom")
@@ -359,7 +480,7 @@ def GatherAllPatientData():
     print("Enter 8: for processed switzerland data")
     print("Enter 9: for a file of your choice")
 
-    while chosen == False:
+    while True:
         choice = int(input("       : "))
         match(choice):
             case 0:
@@ -415,16 +536,19 @@ def Menu():
         print("Enter 4: To detect all missing or incorrct data")
         print("Enter 5: To process wanted data into a CSV file")
         choice = int(input("       : "))
-
-        patientData, fileType = GatherAllPatientData()
+        print()
+        if choice != 0:
+            patientData, fileType = GatherAllPatientData()
         print()
         match(choice):
+            case 0:
+                break 
             case 1:
                 FeatureInfermation(patientData, fileType, 0)
             case 2:
                 FeatureInfermationDeseaseClassifiaction(patientData, fileType, 0)
             case 3:
-                FeatureStatistics(patientData, fileType)
+                FeatureStatistics(patientData, fileType, 0)
             case 4:
                 MissingData(patientData, fileType, 0)
             case 5:
